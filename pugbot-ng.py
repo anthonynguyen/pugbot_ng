27ebd1cb97b5919331023a815955af7c9ba06a9e
Jon Chen <bsd@voltaire.sh>
2014-07-25 20:39:19 -0400

    pep8

--- pugbot-ng.py
+++ pugbot-ng.py
@@ -4,169 +4,173 @@ import irc.bot
 import json
 import random
 
+
 class Pugbot(irc.bot.SingleServerIRCBot):
-	def __init__(self, config):
-		super(Pugbot, self).__init__([(config["server"], config["port"])], config["nick"], config["nick"])
-		self.channel = config["channel"]
-		self.target = self.channel
-		self.cmdPrefixes = config["prefixes"]
-		self.owner = config["owner"]
-		self.password = ""
-		self.pugSize = config["size"]
+    def __init__(self, config):
+        super(Pugbot, self).__init__([(config["server"], config["port"])],
+                                     config["nick"], config["nick"])
+        self.channel = config["channel"]
+        self.target = self.channel
+        self.cmdPrefixes = config["prefixes"]
+        self.owner = config["owner"]
+        self.password = ""
+        self.pugSize = config["size"]
 
-		self.Q = []
-		self.maps = ["abbey", "algiers", "austria", "bohemia", "casa", "docks", "dressingroom", "eagle", "elgin", "kingdom", "kingdom_rc6", "mandolin", "prague", "riyadh", "sanc", "snoppis", "subway", "swim", "thingley", "tunis", "turnpike", "uptown"]
-		self.votes = {}
-	
-	def on_ping(self, conn, ev):
-		self.connection.pong(ev.target)
+        self.Q = []
+        self.maps = ["abbey", "algiers", "austria", "bohemia", "casa", "docks",
+                     "dressingroom", "eagle", "elgin", "kingdom",
+                     "kingdom_rc6", "mandolin", "prague", "riyadh", "sanc",
+                     "snoppis", "subway", "swim", "thingley", "tunis",
+                     "turnpike", "uptown"]
+        self.votes = {}
 
-	def say(self, msg):
-		self.connection.privmsg(self.channel, msg)
+    def on_ping(self, conn, ev):
+        self.connection.pong(ev.target)
 
-	def pm(self, nick, msg):
-		self.connection.privmsg(nick, msg)
-	
-	def reply(self, msg):
-		self.connection.privmsg(self.target, msg)
+    def say(self, msg):
+        self.connection.privmsg(self.channel, msg)
 
-	def on_welcome(self, conn, e):
-		conn.join(self.channel)
+    def pm(self, nick, msg):
+        self.connection.privmsg(nick, msg)
 
-		alpha = "abcdefghijklmopqrstuvwxyz"
-		password = ""
-		for _ in range(5):
-			password += random.choice(alpha)
-		self.password = password
+    def reply(self, msg):
+        self.connection.privmsg(self.target, msg)
 
-		print("The password is: " + password)
-		if self.owner != "":
-			self.pm(self.owner, "The password is: " + password)
+    def on_welcome(self, conn, e):
+        conn.join(self.channel)
 
-	def on_privmsg(self, conn, e):
-		self.executeCommand(conn, e, True)
+        self.password = ''.join(
+            random.choice("abcdefghijklmopqrstuvwxyz") for _ in range(5))
 
-	def on_pubmsg(self, conn, e):
-		if (e.arguments[0][0] in self.cmdPrefixes):
-			self.executeCommand(conn, e)
+        print("The password is: " + self.password)
+        if self.owner != "":
+            self.pm(self.owner, "The password is: " + self.password)
 
-	def executeCommand(self, conn, e, private = False):
-		issuedBy = e.source.nick
-		text = e.arguments[0][1:].split(" ")
-		command = text[0].lower()
-		data = " ".join(text[1:])
+    def on_privmsg(self, conn, e):
+        self.executeCommand(conn, e, True)
 
-		if private:
-			self.target = issuedBy
-		else:
-			self.target = self.channel
+    def on_pubmsg(self, conn, e):
+        if (e.arguments[0][0] in self.cmdPrefixes):
+            self.executeCommand(conn, e)
 
-		try:
-			commandFunc = getattr(self, "cmd_" + command)
-			commandFunc(issuedBy, data)
-		except AttributeError:
-			self.reply("Command not found: " + command)
+    def executeCommand(self, conn, e, private=False):
+        issuedBy = e.source.nick
+        text = e.arguments[0][1:].split(" ")
+        command = text[0].lower()
+        data = " ".join(text[1:])
 
-	def cmd_help(self, issuedBy, data):
-		""".help [command] - displays this message"""
-		if data == "":
-			attrs = sorted(dir(self))
-			self.reply("Commands:")
-			for attr in attrs:
-				if attr[:4] == "cmd_":
-					self.reply(getattr(self, attr).__doc__)
-		else:
-			try:
-				command = getattr(self, "cmd_" + data.lower())
-				self.reply(command.__doc__)
-			except AttributeError:
-				self.reply("Command not found: " + data)
-	
-	def cmd_plzdie(self, issuedBy, data):
-		""".plzdie - kills the bot"""
-		if (data == self.password):
-			self.die("{0} doesn't like me :<".format(issuedBy))
-		else:
-			self.reply("You can't run that command without a password")
-	
-	def cmd_join(self, issuedBy, data):
-		""".join - joins the queue"""
-		if issuedBy not in self.Q:
-			self.Q.append(issuedBy)
-			self.say("{0} was added to the queue".format(issuedBy))
-		else:
-			self.reply("You are already in the queue")
+        if private:
+            self.target = issuedBy
+        else:
+            self.target = self.channel
 
-		if len(self.Q) == self.pugSize:
-			self.say("\x034,2Ding ding ding, the PUG is starting!")
-			self.Q = []
-			self.votes = {}
+        try:
+            commandFunc = getattr(self, "cmd_" + command)
+            commandFunc(issuedBy, data)
+        except AttributeError:
+            self.reply("Command not found: " + command)
 
-	def cmd_leave(self, issuedBy, data):
-		""".leave - leaves the queue"""
-		if issuedBy in self.Q:
-			self.Q.remove(issuedBy)
-			self.say("{0} was removed from the queue".format(issuedBy))
-		else:
-			self.reply("You are not in the queue")
+    def cmd_help(self, issuedBy, data):
+        """.help [command] - displays this message"""
+        if data == "":
+            attrs = sorted(dir(self))
+            self.reply("Commands:")
+            for attr in attrs:
+                if attr[:4] == "cmd_":
+                    self.reply(getattr(self, attr).__doc__)
+        else:
+            try:
+                command = getattr(self, "cmd_" + data.lower())
+                self.reply(command.__doc__)
+            except AttributeError:
+                self.reply("Command not found: " + data)
 
-	def cmd_status(self, issuedBy, data):
-		""".status - displays the queue status"""
-		if len(self.Q) == 0:
-			self.reply("Queue is empty: 0/{0}".format(self.pugSize))
-			return
+    def cmd_plzdie(self, issuedBy, data):
+        """.plzdie - kills the bot"""
+        if (data == self.password):
+            self.die("{0} doesn't like me :<".format(issuedBy))
+        else:
+            self.reply("You can't run that command without a password")
 
-		self.reply("Queue status: {0}/{1}".format(len(self.Q), self.pugSize))
-		queue = ""
-		Qlen = len(self.Q)
-		for i, p in enumerate(self.Q):
-			if i < Qlen - 1:
-				queue += p + ", "
-			else:
-				queue += p
-		self.reply(queue)
+    def cmd_join(self, issuedBy, data):
+        """.join - joins the queue"""
+        if issuedBy not in self.Q:
+            self.Q.append(issuedBy)
+            self.say("{0} was added to the queue".format(issuedBy))
+        else:
+            self.reply("You are already in the queue")
 
-	def cmd_maps(self, issuedBy, data):
-		""".maps - list maps that are able to be voted"""
-		self.say("Maps: abbey, algiers, austria, bohemia, casa, docks, dressingroom, eagle, elgin, kingdom, kingdom_rc6, mandolin, prague, riyadh, sanc, snoppis, subway, swim, thingley, tunis, turnpike, uptown")
+        if len(self.Q) == self.pugSize:
+            self.say("\x034,2Ding ding ding, the PUG is starting!")
+            self.Q = []
+            self.votes = {}
 
-	def cmd_vote(self, issuedBy, data):
-		""".vote - vote for a map"""
-		if issuedBy not in self.Q:
-			self.reply("You are not in the queue")
-		else:
-			if data not in self.maps:
-				self.reply("{0} is not a valid map".format(data))
-		
-			if data in self.maps:
-				self.votes[issuedBy] = data
-				self.say("{0} voted for {1}".format(issuedBy, data))
+    def cmd_leave(self, issuedBy, data):
+        """.leave - leaves the queue"""
+        if issuedBy in self.Q:
+            self.Q.remove(issuedBy)
+            self.say("{0} was removed from the queue".format(issuedBy))
+        else:
+            self.reply("You are not in the queue")
 
-	def cmd_votes(self, issuedBy, data):
-		""".votes - show number of votes per map"""
+    def cmd_status(self, issuedBy, data):
+        """.status - displays the queue status"""
+        if len(self.Q) == 0:
+            self.reply("Queue is empty: 0/{0}".format(self.pugSize))
+            return
 
-		mapvotes = self.votes.values()
-		tallies = dict((map, mapvotes.count(map)) for map in mapvotes)
+        self.reply("Queue status: {0}/{1}".format(len(self.Q), self.pugSize))
+        queue = ""
+        Qlen = len(self.Q)
+        for i, p in enumerate(self.Q):
+            if i < Qlen - 1:
+                queue += p + ", "
+            else:
+                queue += p
+        self.reply(queue)
 
-		if len(self.votes) == 0:
-			self.reply("There are no current votes")
-		else:
-			for map in tallies:
-				self.reply("{0}: {1} vote{2}".format(map, tallies[map], "" if tallies[map] == 1 else "s"))
+    def cmd_maps(self, issuedBy, data):
+        """.maps - list maps that are able to be voted"""
+        self.say("maps: {0}".format(", ".join(self.maps)))
+
+    def cmd_vote(self, issuedBy, data):
+        """.vote - vote for a map"""
+        if issuedBy not in self.Q:
+            self.reply("You are not in the queue")
+        else:
+            if data not in self.maps:
+                self.reply("{0} is not a valid map".format(data))
+
+            if data in self.maps:
+                self.votes[issuedBy] = data
+                self.say("{0} voted for {1}".format(issuedBy, data))
+
+    def cmd_votes(self, issuedBy, data):
+        """.votes - show number of votes per map"""
+
+        mapvotes = self.votes.values()
+        tallies = dict((map, mapvotes.count(map)) for map in mapvotes)
+
+        if len(self.votes) == 0:
+            self.reply("There are no current votes")
+        else:
+            for map in tallies:
+                self.reply("{0}: {1} vote{2}".format(
+                    map, tallies[map], "" if tallies[map] == 1 else "s"))
 
 if __name__ == "__main__":
-	try:
-		configFile = open("config.json", "r")
-		config = json.loads(configFile.read())
-	except IOError:
-		config = {
-			"server": "irc.quakenet.org",
-			"port": 6667,
-			"prefixes": "!@>.",
-			"channel": "#pugbot-ng",
-			"nick": "pugbot-ng",
-			"owner": ""
-		}
+    try:
+        configFile = open("config.json", "r")
+        config = json.loads(configFile.read())
+    except IOError:
+        config = {
+            "server": "irc.quakenet.org",
+            "port": 6667,
+            "prefixes": "!@>.",
+            "channel": "#pugbot-ng",
+            "nick": "pugbot-ng",
+            "owner": ""
+        }
 
-	bot = Pugbot(config)
-	bot.start()
+    bot = Pugbot(config)
+    bot.start()
