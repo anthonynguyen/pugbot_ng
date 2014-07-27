890aac4c0d09c935d4f84357880c5f9121fd8b67
Jon Chen <bsd@voltaire.sh>
2014-07-27 03:37:38 -0400

    init package

--- pugbot_ng/__init__.py
+++ pugbot_ng/__init__.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import, division, print_function
 import irc.bot
 import json
 import logging
 import os
 import random
 
+from .__about__ import (
+    __author__, __copyright__, __email__, __license__, __summary__, __title__,
+    __uri__, __version__
+)
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
 
 __CONFIG = {
     "server": "irc.quakenet.org",
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import setuptools
+
+about = {}
+with open(os.path.join(os.path.base_dir, "pugbot_ng", "__about__.py")) as f:
+    exec(f.read(), about)
+
+with open(os.path.join(os.path.base_dir, "README.rst")) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=about["__title__"],
+    version=about["__version__"],
+
+    description=about["__summary__"],
+    long_description=long_description,
+    license=about["__license__"],
+    url=about["__uri__"],
+
+    author=about["__author__"],
+    author_email=about["__email__"],
+
+    classifiers=[
+        "Intended Audience :: System Adminstrators",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Communications :: Chat :: Internet Relay Chat"
+    ],
+)
