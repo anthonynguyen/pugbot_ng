88a40d760cf7ac043bd9fa94730a59e306e0cc72
Anthony Nguyen <anknguyen@gmail.com>
2014-07-28 17:56:31 -0400

    Updated join command test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -33,10 +33,27 @@ class CommandTest(unittest.TestCase):
         self.handler.cmd_join("user1", "")
         testQueue.append("user1")
         self.assertEqual(self.state.Q, testQueue)
+        self.bot.say.assert_called_with("user1 was added to the queue")
 
+        self.handler.cmd_join("user1", "")
+        self.assertEqual(self.state.Q, testQueue)
+        self.bot.notice.assert_called_with("user1", "You are already in the queue")
+       
         self.handler.cmd_join("user2", "")
         testQueue.append("user2")
         self.assertEqual(self.state.Q, testQueue)
-       
+        self.bot.say.assert_called_with("user2 was added to the queue")
+
+        self.handler.cmd_join("user3", "blah")
+        testQueue.append("user3")
+        self.assertEqual(self.state.Q, testQueue)
+        self.bot.say.assert_called_with("user3 was added to the queue")
+        self.bot.notice.assert_called_with("user3", "blah is not a valid map")
+
+        self.handler.cmd_join("user3", "town")
+        self.assertEqual(self.state.Q, testQueue)
+        self.bot.say.assert_called_with("user3 voted for uptown")
+        self.assertEqual(self.state.votes, {"user3": "uptown"})
+
 if __name__ == "__main__":
     unittest.main()
